378d8eee4c0cb24923c0d13d5474b43061864bdc
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2014-07-01 23:05:38 +0900

    Allow saying name option

--- masa.sh
+++ masa.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 voices=`say -v ?`
-masa="Takahashi Masamitsu"
+if [ $# -eq 0 ]; then
+  name="Takahashi Masamitsu"
+else
+  name=$1
+fi
 echo "$voices" | while read line; do
   array=($line)
   who=${array[0]}
-  echo "$who says. $masa"
-  say -v "$who" -r 10 $masa
+  echo "$who says. $name"
+  say -v "$who" -r 10 $name
 done
